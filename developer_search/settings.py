4872de342c68f497c499b0c7efca1e10caed107d
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2021-08-08 17:32:18 +0530

    Changes

--- developer_search/settings.py
+++ developer_search/settings.py
@@ -148,8 +148,8 @@ django_heroku.settings(locals())
 
 if production:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
     SECURE_SSL_REDIRECT = True
 else:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
